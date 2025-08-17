9faa63d2140b49595154be8a978b1ba5b1317b29
suraj <nagisetty.suraj@gmail.com>
2025-08-17 12:22:51 +0530

    adding integration tests

--- Makefile
+++ Makefile
@@ -1,5 +1,8 @@
 .PHONY: default tests
 
+export PYTHONPATH = $(realpath ./)
+export ULEDGER3_SCRIPTS = $(realpath ./uledger3-scripts)
+
 default: tests
 
 tests:
---
+++ tests/integration/utils.sh
@@ -0,0 +1,30 @@
+yell() { echo -e "\n❌ ERROR"; echo "$0: $*" >&2; }
+affirm() { echo -e "\n✅ AFFIRM"; echo "$0: $*" >&2; }
+warn() { echo -e "\n⚠️  WARNING"; echo "$0: $*" >&2; }
+info() { echo -e "\nℹ️  INFO"; echo "$0: $*" >&2; }
+die() { yell "$*"; exit 111; }
+try() { "$@" || die "cannot $*"; }
+
+redirect() { a="$1"; shift; "$@" > "$a"; }
+silent() { redirect /dev/null "$@"; }
+
+run_and_expect_error() {
+  local command="$1"
+  local error="$2"
+
+  local output
+  output=$(eval "$command" 2>&1 > /dev/null)
+  local status=$?
+
+  # Check if exit status is non-zero (usually an unhandled exception exits with non-zero)
+  if [ $status -eq 0 ]; then
+    yell "$command exited with status 0, expected failure with $error."
+    return 1
+  fi
+
+  if echo "$output" | grep -q "$error"; then
+    :
+  else
+    die "$command did not raise $error."
+  fi
+}
